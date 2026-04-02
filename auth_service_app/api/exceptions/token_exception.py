ac8b2fc54453edc72f11ae4eb85f9e219444f8ed
Dilip Sharma <diliplakshya@gmail.com>
2026-04-02 22:48:15 +0530

    Remove old exceptions

--- auth_service_app/api/exceptions/token_exception.py
+++
@@ -1,5 +0,0 @@
-from api.exceptions.base_exception import BaseException
-
-class InvalidTokenException(BaseException):
-    def __init__(self, message: str = None):
-        super().__init__(status_code=401, message="Invalid or expired token.")
