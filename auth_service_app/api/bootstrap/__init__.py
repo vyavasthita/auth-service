9f1b41d33b9f4b6d85ea41b5030bafffcb27d8b0
Dilip Sharma <diliplakshya@gmail.com>
2026-04-02 22:46:48 +0530

    Remove bootstrap files

--- auth_service_app/api/bootstrap/__init__.py
+++
@@ -1,5 +0,0 @@
-from .instrumentation_setup import InstrumentationSetup
-from .app_factory import AppFactory
-from .cors_registrar import CORSRegistrar
-from .router_registrar import RouterRegistrar
-from .app_initializer import AppInitializer
